0c42143af3d69a7a99f9c16172655c685e7dff8c
Qasim Iqbal <me@qas.im>
2016-01-06 16:15:19 -0500

    Add InfoPopupView file

---
+++ CDFLabs/Views/InfoPopupView.swift
@@ -0,0 +1,13 @@
+//
+//  InfoPopupView.swift
+//  CDFLabs
+//
+//  Created by Qasim Iqbal on 1/6/16.
+//  Copyright © 2016 Qasim Iqbal. All rights reserved.
+//
+
+import UIKit
+
+class InfoPopupView: UIView {
+    
+}
