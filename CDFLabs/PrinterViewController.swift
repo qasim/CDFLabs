e700f4f42f5de6b02dfb6b73d907bd044f69135c
Qasim Iqbal <me@qas.im>
2016-04-01 16:19:54 -0400

    Start Printer view controller

---
+++ CDFLabs/PrinterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PrinterViewController.swift
+//  CDFLabs
+//
+//  Created by Qasim on 2016-04-01.
+//  Copyright © 2016 Qasim Iqbal. All rights reserved.
+//
+
+import UIKit
+import Just
+import KLCPopup
+
+class PrinterViewController: UIViewController {
+    override func loadView() {
+        super.loadView()
+    }
+}
